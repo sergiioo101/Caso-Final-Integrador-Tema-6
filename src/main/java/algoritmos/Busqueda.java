f1ead24a619d968e9d6c9bdddba2f36f26e5c10a
sergiioo101 <smartros@myuax.com>
2024-04-20 18:33:00 +0200

    4

---
+++ src/main/java/algoritmos/Busqueda.java
@@ -0,0 +1,22 @@
+package algoritmos;
+
+public class Busqueda {
+    public static int busquedaBinaria(Comparable[] arr, Comparable elemento) {
+        int izquierda = 0;
+        int derecha = arr.length - 1;
+
+        while (izquierda <= derecha) {
+            int medio = (izquierda + derecha) / 2;
+            int comparacion = arr[medio].compareTo(elemento);
+
+            if (comparacion == 0) {
+                return medio;
+            } else if (comparacion < 0) {
+                izquierda = medio + 1;
+            } else {
+                derecha = medio - 1;
+            }
+        }
+        return -1; // Si no se encuentra el elemento
+    }
+}
