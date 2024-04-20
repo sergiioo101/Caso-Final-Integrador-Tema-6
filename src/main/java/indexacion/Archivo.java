edf5516f62ee85f7166d8dd0945cb3bef655d65b
sergiioo101 <smartros@myuax.com>
2024-04-20 18:37:38 +0200

    7

---
+++ src/main/java/indexacion/Archivo.java
@@ -0,0 +1,44 @@
+package indexacion;
+
+import java.io.File;
+import java.util.Date;
+
+public class Archivo {
+    private String nombre;
+    private String ruta;
+    private long tamaño;
+    private Date fechaModificacion;
+
+    public Archivo(String ruta) {
+        File archivo = new File(ruta);
+        if (!archivo.exists()) {
+            throw new IllegalArgumentException("El archivo especificado no existe.");
+        }
+        this.nombre = archivo.getName();
+        this.ruta = archivo.getAbsolutePath();
+        this.tamaño = archivo.length();
+        this.fechaModificacion = new Date(archivo.lastModified());
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getRuta() {
+        return ruta;
+    }
+
+    public long getTamaño() {
+        return tamaño;
+    }
+
+    public Date getFechaModificacion() {
+        return fechaModificacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + ", Ruta: " + ruta + ", Tamaño: " + tamaño + " bytes, Fecha de Modificación: " + fechaModificacion;
+    }
+}
+
